7aa3e548bb1a4c99bf408ee84ef09c8914affad4
Terry <marteresa28@gmail.com>
2016-04-26 21:26:52 +0200

    codice

--- File_python/EDF_UFFICI.py
+++ File_python/EDF_UFFICI.py
@@ -15,8 +15,8 @@ from larlib import *
 lines = lines2lines("rialzo_level1.lines")
 V,FV,EV,polygons = larFromLines(lines) #taglia le parti di troppo
 grafo=STRUCT(MKPOLS((V,EV))) #grafo pulito
-VV = AA(LIST)(range(len(V)))
-submodel = STRUCT(MKPOLS((V,EV)))
+#VV = AA(LIST)(range(len(V)))
+#submodel = STRUCT(MKPOLS((V,EV)))
 #VIEW(larModelNumbering(1,1,1)(V,[VV,EV,FV],submodel,0.1))
 
 #V[6] da portare nell'origine, spigolo EV[7] da portare a grandezza mondo: 42metri
@@ -29,7 +29,7 @@ scala = 41.9/0.9949
 
 W=((mat(V)-V[6])*scala).tolist()
 
-#***
+
 #**verifica** 
 #WW = AA(LIST)(range(len(W)))
 #VIEW(larModelNumbering(1,1,1)(W,[WW,EV,FV],STRUCT(MKPOLS((W,EV))),2))
@@ -42,22 +42,17 @@ W=((mat(V)-V[6])*scala).tolist()
 buchi = STRUCT(MKPOLS((W,[FV[0]])))
 tot = STRUCT(MKPOLS([W,FV]))
 base0= DIFFERENCE([tot,buchi])
-VIEW(base0)
-***
-
+#VIEW(base0)
 rialzo=PROD([base0,Q(1.5)])
 #VIEW(rialzo)
-
-
-
  
 #**LEVEL 1_muri esterni** 
 
 lines = lines2lines("rialzo_level1.lines")
 V,FV,EV,polygons = larFromLines(lines) #taglia le parti di troppo
 grafo=STRUCT(MKPOLS((V,EV))) #grafo pulito
-VV = AA(LIST)(range(len(V)))
-submodel = STRUCT(MKPOLS((V,EV)))
+#VV = AA(LIST)(range(len(V)))
+#submodel = STRUCT(MKPOLS((V,EV)))
 #VIEW(larModelNumbering(1,1,1)(V,[VV,EV,FV],submodel,0.1))
 
 #V[6] da portare nell'origine, spigolo EV[7] da portare a grandezza mondo: 42metri
@@ -75,12 +70,12 @@ EV_bassi=sorted([1,3,5,0,10,12,16,7])
 EV_alti= set(range(len(EV))).difference(EV_bassi)
 muri_bassi=STRUCT(AA(POLYLINE)([[W[EV[e][0]],W[EV[e][1]]] for e in EV_bassi]))
 muri_alti=STRUCT(AA(POLYLINE)([[W[EV[e][0]],W[EV[e][1]]] for e in EV_alti]))
-#***
+
 #**verifica** 
 #WW = AA(LIST)(range(len(W)))
 #VIEW(larModelNumbering(1,1,1)(W,[WW,EV,FV],STRUCT(MKPOLS((W,EV))),2))
 #assert W[9][0]-W[3][0]==41.9
-***
+
 #SPIGOLo 2 diverso
 EVmuri=set(range(len(EV))).difference({2})
 muro = STRUCT(MKPOLS((W,[EV[2]])))
@@ -103,8 +98,8 @@ V,FV,EV,polygons = larFromLines(lines) #taglia le parti di troppo
 grafo=STRUCT(MKPOLS((V,EV))) #grafo pulito
 #VIEW(OFFSET([0.005,0.005])(grafo))
 
-VV = AA(LIST)(range(len(V)))
-submodel = STRUCT(MKPOLS((V,EV)))
+#VV = AA(LIST)(range(len(V)))
+#submodel = STRUCT(MKPOLS((V,EV)))
 #VIEW(larModelNumbering(1,1,1)(V,[VV,EV,FV],submodel,0.1))
 
 #V[2] da portare nell'origine, spigolo EV[9] da portare a grandezza mondo: 42metri
@@ -121,12 +116,11 @@ W=((mat(V)-V[2])*scala).tolist()
 EV_no=sorted([0,1,7,5,10,2,3,9])
 EV_si= set(range(len(EV))).difference(EV_no)
 
-#***
 #**verifica** 
 #WW = AA(LIST)(range(len(W)))
 #VIEW(larModelNumbering(1,1,1)(W,[WW,EV,FV],STRUCT(MKPOLS((W,EV))),2))
 #assert W[7][0]-W[3][0]==41.9
-***
+
 
 baseC=STRUCT(AA(POLYLINE)([[W[EV[e][0]],W[EV[e][1]]] for e in EV_si]))
 baseC=OFFSET([0.3,0.3])(baseC)
@@ -144,8 +138,8 @@ grafo = STRUCT(AA(POLYLINE)(lines))
 
 #numerazione vertici e spigoli
 V,EV = lines2lar(lines)
-VV = AA(LIST)(range(len(V)))
-submodel = STRUCT(MKPOLS((V,EV)))
+#VV = AA(LIST)(range(len(V)))
+#submodel = STRUCT(MKPOLS((V,EV)))
 #VIEW(larModelNumbering(1,1,1)(V,[VV,EV],submodel,0.05))
 
 #V[3] da portare nell'origine, spigolo V[18]V[21] da portare a grandezza mondo: 42metri
@@ -172,7 +166,7 @@ level_1=STRUCT([wall_1,wall_2])
 level_1=T(3)(1.5)(level_1)
 #VIEW(level_1)
 
-VIEW(STRUCT([rialzo,level1,colonne,level_1]))
+#VIEW(STRUCT([rialzo,level1,colonne,level_1]))
 
 #**LEVEL 2**
 lines = lines2lines("uff_level2.lines")
@@ -181,20 +175,18 @@ grafo = STRUCT(AA(POLYLINE)(lines))
 
 #numerazione vertici e spigoli
 V,EV = lines2lar(lines)
-VV = AA(LIST)(range(len(V)))
-submodel = STRUCT(MKPOLS((V,EV)))
+#VV = AA(LIST)(range(len(V)))
+#submodel = STRUCT(MKPOLS((V,EV)))
 #VIEW(larModelNumbering(1,1,1)(V,[VV,EV],submodel,0.2))
 
 #V[40] da portare nell'origine, spigolo V[40]V[23] da portare a grandezza mondo: 42metri
-assert V[40]==[0.0, 0.5928]
-assert V[23]==[1.0, 0.5928]
-assert V[40][0]-V[23][0] == 1.0
+assert V[39]==[0.0, 0.5928]
 scala = 41.9
 
-W=((mat(V)-V[40])*scala).tolist()
+W=((mat(V)-V[39])*scala).tolist()
 
 #muri fini e muri spessi
-EV_spessi=sorted([28,1,7,38,0,27,32,45,42,8,9,34,13,44,16])
+EV_spessi=sorted([23,1,34,14,22,37,7,30,19,25,17,15,16,35,26,41,18,36,3,13,45,6])
 EV_fini= set(range(len(EV))).difference(EV_spessi)
 muri_spessi=STRUCT(AA(POLYLINE)([[W[EV[e][0]],W[EV[e][1]]] for e in EV_spessi]))
 muri_fini=STRUCT(AA(POLYLINE)([[W[EV[e][0]],W[EV[e][1]]] for e in EV_fini]))
@@ -209,18 +201,18 @@ level_2=STRUCT([wall_1,wall_2])
 level_2=T(3)(4.5)(level_2)
 #VIEW(level_2)
 
-VIEW(STRUCT([rialzo,level1,colonne,level_1,level_2]))
+#VIEW(STRUCT([rialzo,level1,colonne,level_1,level_2]))
 
 #**LEVEL 3**
 
 lines = lines2lines("uff_level3.lines")
-grafo = STRUCT(AA(POLYLINE)(lines))
+#grafo = STRUCT(AA(POLYLINE)(lines))
 #VIEW(grafo)
 
 #numerazione vertici e spigoli
 V,EV = lines2lar(lines)
-VV = AA(LIST)(range(len(V)))
-submodel = STRUCT(MKPOLS((V,EV)))
+#VV = AA(LIST)(range(len(V)))
+#submodel = STRUCT(MKPOLS((V,EV)))
 #VIEW(larModelNumbering(1,1,1)(V,[VV,EV],submodel,0.2))
 
 #V[38] da portare nell'origine, spigolo V[38]V[22] da portare a grandezza mondo: 42metri
@@ -251,18 +243,18 @@ level_3up=STRUCT(NN(4)([level_3,T(3)(6)]))
 #VIEW(level_3)
 #VIEW(level_3up)
 
-VIEW(STRUCT([rialzo,level1,colonne,level_1,level_2,level_3,level_3up]))
+#VIEW(STRUCT([rialzo,level1,colonne,level_1,level_2,level_3,level_3up]))
 
 #**LEVEL 4**
 
 lines = lines2lines("uff_level4.lines")
-grafo = STRUCT(AA(POLYLINE)(lines))
+#grafo = STRUCT(AA(POLYLINE)(lines))
 #VIEW(grafo)
 
 #numerazione vertici e spigoli
 V,EV = lines2lar(lines)
-VV = AA(LIST)(range(len(V)))
-submodel = STRUCT(MKPOLS((V,EV)))
+#VV = AA(LIST)(range(len(V)))
+#submodel = STRUCT(MKPOLS((V,EV)))
 #VIEW(larModelNumbering(1,1,1)(V,[VV,EV],submodel,0.2))
 
 #V[42] da portare nell'origine, spigolo V[42]V[27] da portare a grandezza mondo: 42metri
@@ -296,29 +288,28 @@ grafo = STRUCT(AA(POLYLINE)(lines))
 
 #numerazione vertici e spigoli
 V,EV = lines2lar(lines)
-VV = AA(LIST)(range(len(V)))
-submodel = STRUCT(MKPOLS((V,EV)))
+#VV = AA(LIST)(range(len(V)))
+#submodel = STRUCT(MKPOLS((V,EV)))
 #VIEW(larModelNumbering(1,1,1)(V,[VV,EV],submodel,0.2))
 W=((mat(V)-V[5])*scala).tolist()
 base=STRUCT(MKPOLS((W,EV)))
 finale=OFFSET([0.3,0.3])(base)
 finale=T(3)(28.5)(PROD([finale,Q(5.5)]))
 
-#dettagli
-***
+#dettagli righe
+
 lines = lines2lines("rialzo_level1.lines")
 V,FV,EV,polygons = larFromLines(lines) #taglia le parti di troppo
-grafo=STRUCT(MKPOLS((V,EV))) #grafo pulito
-VV = AA(LIST)(range(len(V)))
-submodel = STRUCT(MKPOLS((V,EV)))
+#grafo=STRUCT(MKPOLS((V,EV))) #grafo pulito
+#VV = AA(LIST)(range(len(V)))
+#submodel = STRUCT(MKPOLS((V,EV)))
 #VIEW(larModelNumbering(1,1,1)(V,[VV,EV,FV],submodel,0.1))
 
 scala = 41.9/0.9949
 
 W=((mat(V)-V[6])*scala).tolist()
 
-#***
-#**verifica** 
+#verifica 
 #WW = AA(LIST)(range(len(W)))
 #VIEW(larModelNumbering(1,1,1)(W,[WW,EV,FV],STRUCT(MKPOLS((W,EV))),2))
 #assert W[9][0]-W[3][0]==41.9
@@ -335,7 +326,7 @@ rip_MARMO=STRUCT(NN(11)([MARMO,T(3)(3)]))
 #VIEW(level_4)
 #VIEW(level_4up)
 EDF_UFF=STRUCT([rialzo,level1,colonne,level_1,level_2,level_3up,level_4up,finale,rip_MARMO])
-VIEW(EDF_UFF)
+#VIEW(EDF_UFF)
 
 #**PAVIMENTI**
 #**pavimento level 2**
@@ -344,16 +335,16 @@ Y,FV,EV,polygons = larFromLines(lines) #taglia le parti di troppo
 #prova=STRUCT(MKPOLS((V,FV))) #grafo pulito
 #VIEW(prova)
 
-YY = AA(LIST)(range(len(Y)))
-submodel = STRUCT(MKPOLS((Y,EV)))
+#YY = AA(LIST)(range(len(Y)))
+#submodel = STRUCT(MKPOLS((Y,EV)))
 #VIEW(larModelNumbering(1,1,1)(Y,[YY,EV,FV],submodel,0.1))
 #V[26] da portare nell'origine, spigolo EV[18] da portare a grandezza mondo: 42metri
 assert Y[26]==[0.0, 0.5928]
 assert Y[26][0]-Y[7][0] == 1.0
 scala = 41.9
 Z=((mat(Y)-Y[26])*scala).tolist()
-ZZ = AA(LIST)(range(len(Z)))
-submodel = STRUCT(MKPOLS((Z,EV)))
+#ZZ = AA(LIST)(range(len(Z)))
+#submodel = STRUCT(MKPOLS((Z,EV)))
 #VIEW(larModelNumbering(1,1,1)(Z,[ZZ,EV,FV],submodel,0.1))
 #**celle vuote**
 #celle vuote sono 0,1,2,7,65,4, mi serve solo la 3
@@ -362,7 +353,7 @@ tot = STRUCT(MKPOLS([Z,FV]))
 pavimento_level2= DIFFERENCE([tot,buchi])
 pavimento_level2=T(3)(4.25)(PROD([pavimento_level2,Q(0.3)]))
 #VIEW(pavimento_level2)
-VIEW(STRUCT([EDF_UFF,pavimento_level2]))
+#VIEW(STRUCT([EDF_UFF,pavimento_level2]))
 
 
 #**pavimento level3**
@@ -371,8 +362,8 @@ V,FV,EV,polygons = larFromLines(lines) #taglia le parti di troppo
 #prova=STRUCT(MKPOLS((V,FV))) #grafo pulito
 #VIEW(prova)
 
-VV = AA(LIST)(range(len(V)))
-submodel = STRUCT(MKPOLS((V,EV)))
+#VV = AA(LIST)(range(len(V)))
+#submodel = STRUCT(MKPOLS((V,EV)))
 #VIEW(larModelNumbering(1,1,1)(V,[VV,EV,FV],submodel,0.1))
 #V[21] da portare nell'origine a grandezza mondo: 42metri
 assert V[21]==[0.0, 0.5928]
@@ -385,7 +376,7 @@ tot = STRUCT(MKPOLS([W,FV]))
 pavimento_level3= DIFFERENCE([tot,buchi])
 pavimento_level3=T(3)(7.25)(PROD([pavimento_level3,Q(0.3)]))
 #VIEW(pavimento_level3)
-VIEW(STRUCT([EDF_UFF,pavimento_level3,pavimento_level2]))
+#VIEW(STRUCT([EDF_UFF,pavimento_level3,pavimento_level2]))
 
 #**pavimento level4**
 lines = lines2lines("pavimento_level3.lines")
@@ -393,8 +384,8 @@ V,FV,EV,polygons = larFromLines(lines) #taglia le parti di troppo
 #prova=STRUCT(MKPOLS((V,FV))) #grafo pulito
 #VIEW(prova)
 
-VV = AA(LIST)(range(len(V)))
-submodel = STRUCT(MKPOLS((V,EV)))
+#VV = AA(LIST)(range(len(V)))
+#submodel = STRUCT(MKPOLS((V,EV)))
 #VIEW(larModelNumbering(1,1,1)(V,[VV,EV,FV],submodel,0.1))
 #V[21] da portare nell'origine a grandezza mondo: 42metri
 assert V[21]==[0.0, 0.5928]
@@ -416,8 +407,8 @@ V,FV,EV,polygons = larFromLines(lines) #taglia le parti di troppo
 #prova=STRUCT(MKPOLS((V,FV))) #grafo pulito
 #VIEW(prova)
 
-VV = AA(LIST)(range(len(V)))
-submodel = STRUCT(MKPOLS((V,EV)))
+#VV = AA(LIST)(range(len(V)))
+#submodel = STRUCT(MKPOLS((V,EV)))
 #VIEW(larModelNumbering(1,1,1)(V,[VV,EV,FV],submodel,0.1))
 #V[21] da portare nell'origine a grandezza mondo: 42metri
 assert V[21]==[0.0, 0.5928]
@@ -433,7 +424,7 @@ top2=T(3)(31.25)(PROD([top,Q(0.3)]))
 PAVIMENTI_UFF=COLOR(RED)(STRUCT([pavimento_3up,pavimento_level2,pavimento_4up,top1,top2]))
 
 #VIEW(pavimento_level4up)
-VIEW(STRUCT([EDF_UFF,PAVIMENTI_UFF]))
+#VIEW(STRUCT([EDF_UFF,PAVIMENTI_UFF]))
 
 #**MURO CIRCOLARE**
 
@@ -443,8 +434,8 @@ grafo = STRUCT(AA(POLYLINE)(lines))
 
 #numerazione vertici e spigoli
 V,EV = lines2lar(lines)
-VV = AA(LIST)(range(len(V)))
-submodel = STRUCT(MKPOLS((V,EV)))
+#VV = AA(LIST)(range(len(V)))
+#submodel = STRUCT(MKPOLS((V,EV)))
 #VIEW(larModelNumbering(1,1,1)(V,[VV,EV],submodel,0.2))
 
 #lo spigolo EV[60] deve misurare (misura presa dal pavimento livello 2) W[12][0]-W[4][0]==7.366019999999999, traslazione Z[4]
@@ -452,8 +443,8 @@ submodel = STRUCT(MKPOLS((V,EV)))
 scaling=7.366019999999999
 W=((mat(V)-V[64])*scaling).tolist()
 W=(mat(W)+Z[4]).tolist()
-WW = AA(LIST)(range(len(W)))
-submodel = STRUCT(MKPOLS((W,EV)))
+#WW = AA(LIST)(range(len(W)))
+#submodel = STRUCT(MKPOLS((W,EV)))
 #VIEW(larModelNumbering(1,1,1)(W,[WW,EV],submodel,0.2))
 
 
@@ -462,8 +453,18 @@ EV_si= set(range(len(EV))).difference(EV_no)
 muro_circ=STRUCT(AA(POLYLINE)([[W[EV[e][0]],W[EV[e][1]]] for e in EV_si]))
 
 muro_circ=OFFSET([0.3,0.3])(muro_circ)
-muro_circ=T(3)(1)(PROD([muro_circ,Q(33)]))
-VIEW(STRUCT([EDF_UFF,PAVIMENTI_UFF,muro_circ]))
+muro_circ=T([2,3])([0.1,1])(PROD([muro_circ,Q(33)]))
+#VIEW(STRUCT([EDF_UFF,PAVIMENTI_UFF,muro_circ]))
+
+#righe circolari
+muro_circ1=STRUCT(AA(POLYLINE)([[W[EV[e][0]],W[EV[e][1]]] for e in EV_si]))
+righe_circ = T([1,2])([-0.15,-0.15])( OFFSET([0.60,0.60])(muro_circ1))
+RIGHE_circ=PROD([righe_circ,INTERVALS(0.20)(1)])
+MARMO_circ=T(3)(1.4)(RIGHE_circ)
+rip_MARMOcirc=STRUCT(NN(11)([MARMO_circ,T(3)(3)]))
+
+EDF_UFF=STRUCT([rialzo,level1,colonne,level_1,level_2,level_3up,level_4up,finale,rip_MARMO,rip_MARMOcirc])
+
 Pav_UFF=T([1,2])([0.1,0.2])(S(2)(0.99)(PAVIMENTI_UFF))
 
 #BUCHI e DETTAGLI
@@ -476,12 +477,12 @@ taglio=T(1)(21.2)(taglio)
 #larghezza colonna W[10][0]-W[1][0] Out[376]: 9.674709999999997
 #distanza dei rettangoli da inizio è di 0.8
 rett = CUBOID([8,1,12.5])
-rett=T([1,2,3])([33.4,-0.5,2.25])(rett)
+rett=T([1,2,3])([33.4,-0.3,2.25])(rett)
 #EDF_UFF2=DIFFERENCE([EDF_UFF1,rett])
 #VIEW(STRUCT([EDF_UFF2,PAVIMENTI_UFF,muro_circ]))
 
 rett1 = CUBOID([8,1,12.5])
-rett1=T([1,2,3])([0.8,-0.5,2.25])(rett1)
+rett1=T([1,2,3])([0.8,-0.3,2.25])(rett1)
 #EDF_UFF3=DIFFERENCE([EDF_UFF2,rett1])
 #VIEW(STRUCT([EDF_UFF3,PAVIMENTI_UFF,muro_circ]))
 
@@ -493,11 +494,11 @@ triangle=S([1,2])([4,13.5])(triangle)
 triangle=PROD([triangle,INTERVALS(1)(1)])
 #VIEW(triangle)
 #cambio di coordinate con lo scambio e poi buco  
-Q,FQ,EQ=UKPOL(triangle)
-Q = AA(CONS([S1,S3,S2]))(Q)
-tr_buco=STRUCT([MKPOL((Q,FQ,EQ))])
+X,FX,EX=UKPOL(triangle)
+X = AA(CONS([S1,S3,S2]))(X)
+tr_buco=STRUCT([MKPOL((X,FX,EX))])
 #altezza 16.5
-tr_buco=T([1,2,3])([37.4,-0.5,18])(tr_buco)
+tr_buco=T([1,2,3])([37.4,-0.3,18])(tr_buco)
 #EDF_UFF4=DIFFERENCE([EDF_UFF3,tr_buco])
 #VIEW(STRUCT([EDF_UFF4,PAVIMENTI_UFF,muro_circ]))
 #secondo
@@ -512,7 +513,7 @@ V,FV,EV=UKPOL(triangle)
 V = AA(CONS([S1,S3,S2]))(V)
 tr_buco1=STRUCT([MKPOL((V,FV,EV))])
 #altezza 16.5
-tr_buco1=COLOR(RED)(T([1,2,3])([4.8,-0.5,18])(tr_buco1))
+tr_buco1=COLOR(RED)(T([1,2,3])([4.8,-0.3,18])(tr_buco1))
 #EDF_UFF5=DIFFERENCE([EDF_UFF4,tr_buco1])
 #VIEW(STRUCT([EDF_UFF5,PAVIMENTI_UFF,muro_circ]))
 
@@ -521,13 +522,33 @@ buco_circ= CYLINDER([4,3])(100)
 #VIEW(buco_circ)
 #12 altezza, 5.3 
 buco=R([1,3])(PI/2)(buco_circ)
-circ=T([1,2,3])([0.5,4.8,16.5])(buco)
+circ=T([1,2,3])([1,4.8,16.5])(buco)
 #VIEW(STRUCT([circ,EDF_UFF5]))
 #12 altezza, 5.3 
 circ2=T(1)(42)(circ)
 
 BUCHI=COLOR(RED)(STRUCT([taglio,rett,rett1,tr_buco,tr_buco1,circ,circ2]))
 EDF_UFFfinal=DIFFERENCE([EDF_UFF,BUCHI])
-VIEW(STRUCT([EDF_UFFfinal,Pav_UFF,muro_circ]))
+#VIEW(STRUCT([EDF_UFFfinal,Pav_UFF,muro_circ]))
 
+#GRIGLIE
+frame=S([1,2])([0.30,0.30])(SKEL_1(STRUCT(MKPOLS(larCuboids([32,32])))))
+grid=OFFSET([0.03,0.03])(frame)
+GRID=PROD([grid,INTERVALS(0.1)(1)])
+frame2=S([1,2])([1.2,1.2])(SKEL_1(STRUCT(MKPOLS(larCuboids([8,8])))))
+grid2=OFFSET([0.1,0.1])(frame2)
+GRID2=PROD([grid2,INTERVALS(0.1)(1)])
+GR=STRUCT([GRID,GRID2])
+GRID_sx=T(3)(33.8)(GR)
+GRID_dx=T(1)(32.4)(GRID_sx)
 
+frame3=S([1,2])([0.41,0.41])(SKEL_1(STRUCT(MKPOLS(larCuboids([7,7])))))
+grid3=OFFSET([0.1,0.1])(frame3)
+GRID3=PROD([grid3,INTERVALS(0.1)(1)])
+grcol_dx=T([1,2,3])([27.997909337621874, 10.928786812744997,33.8])(GRID3)
+grcol_sx=T([1,2,3])([10.928786812744999, 10.928786812744997,33.8])(GRID3)
+GRID_tot=T(2)(0.1)(STRUCT([GRID_dx,GRID_sx,grcol_dx,grcol_sx]))
+EDIFICIO=STRUCT([COLOR(BROWN)(EDF_UFFfinal),Pav_UFF,muro_circ,GRID_tot])
+VIEW(EDIFICIO)
+
+#mancano solo le scale 
